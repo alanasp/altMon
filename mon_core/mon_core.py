8e9f4cd00714da9978d25dc1eaab75cf3ed0a042
Alanas Plascinskas <alanas.pla@gmail.com>
2019-03-27 01:33:23 +0000

    connection to osm kafka

--- mon_core/mon_core.py
+++ mon_core/mon_core.py
@@ -7,12 +7,14 @@ import json
 from decision_engine_ewma import DecisionEngines
 
 
-def feed_measures(dec_engines, measure_msgs):
+def feed_measures(dec_engines, measure_msgs, export, export_producer, export_topic):
     for key in measure_msgs:
         partition = measure_msgs[key]
         for record in partition:
             msg = record.value
             print('Message received: {}'.format(msg))
+            if export:
+                export_producer.send(export_topic, msg)
             vnf = msg['vnf_name']
             metric = msg['metric']
             value = msg['value']
@@ -37,6 +39,21 @@ def get_decision_msgs(dec_engines, config_dict, init=False):
     return decision_msgs
 
 
+def get_export_status(osm_export_msgs):
+    returned_status = None
+    for key in osm_export_msgs:
+        partition = osm_export_msgs[key]
+        for record in partition:
+            msg = record.value
+            if 'export_kind' in msg:
+                if msg['export_kind'] == 'full-export':
+                    return True
+                elif msg['export_kind'] == 'no-export':
+                    return False
+
+    return returned_status
+
+
 print('Loading configurations...')
 # load configurations
 with open('mon_core.config', 'r') as config_file:
@@ -44,15 +61,25 @@ with open('mon_core.config', 'r') as config_file:
 
 print('Configurations loaded!')
 
-print('Connecting to Kafka cluster...')
+print('Connecting to external Kafka cluster at {}...'.format(config['kafka_ext']))
 
-admin_producer = KafkaProducer(bootstrap_servers=config['bootstrap_servers'],
+admin_producer = KafkaProducer(bootstrap_servers=config['kafka_ext'],
                                value_serializer=lambda m: json.dumps(m, sort_keys=True).encode('ascii'))
 
-data_consumer = KafkaConsumer(config['data_topic'], bootstrap_servers=config['bootstrap_servers'],
+data_consumer = KafkaConsumer(config['data_topic'], bootstrap_servers=config['kafka_ext'],
                               value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
-print('Connected to Kafka cluster')
+print('Connected to external Kafka cluster')
+
+print('Connecting to OSM Kafka cluster at {}...'.format(config['kafka_osm']))
+
+osm_producer = KafkaProducer(bootstrap_servers=config['kafka_osm'],
+                             value_serializer=lambda m: json.dumps(m, sort_keys=True).encode('ascii'))
+
+osm_consumer = KafkaConsumer(config['osm_mon_config_topic'], bootstrap_servers=config['kafka_osm'],
+                             value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+print('Connected to OSM Kafka cluster')
 
 DEs = DecisionEngines()
 # create decision engines for all metrics in all VNFs
@@ -74,11 +101,20 @@ for msg in init_admin_msgs:
     print('Sending message: {}'.format(msg))
     admin_producer.send(config['admin_topic'], msg)
 
+export_mon_data = False
+
 while True:
+    osm_msgs = osm_consumer.poll()
+    export_status = get_export_status(osm_msgs)
+    if export_status is not None:
+        export_mon_data = export_status
+
     measures = data_consumer.poll()
-    feed_measures(DEs, measures)
+    feed_measures(DEs, measures, export_mon_data, osm_producer, config['osm_mon_export_topic'])
     admin_msgs = get_decision_msgs(DEs, config)
     for msg in admin_msgs:
         print('Sending message: {}'.format(msg))
+        if export_mon_data:
+            osm_producer.send(config['osm_mon_export_topic'], msg)
         admin_producer.send(config['admin_topic'], msg)
     time.sleep(0.5)
